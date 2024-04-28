d303d6605e4f85b169675aa8fd31f07510ee2335
te-dious <tanmay@qoala.id>
2024-04-28 11:22:20 +0530

    fix

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=150)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    email = models.EmailField(blank=True)
-    date_joined = models.DateTimeField()
-    is_vendor = models.BooleanField()
-    role = models.BooleanField()
+class User(AbstractUser):
+    settings = models.JSONField(default=dict)
