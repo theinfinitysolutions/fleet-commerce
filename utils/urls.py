bc6b9ea0571e044bbb3bbd605590d9ba1e817d44
te-dious <tanmay@qoala.id>
2024-05-18 23:45:16 +0530

    add location url

--- utils/urls.py
+++ utils/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from utils.views import FileObjectView
+from utils.views import FileObjectView, LocationView
 
 urlpatterns = [
     path("file/<int:pk>/", FileObjectView.as_view(), name="file"),
     path("file/", FileObjectView.as_view(), name="file-create"),
+    path("location/", LocationView.as_view(), name="file-create"),
+    path("location/<int:pk>/", LocationView.as_view(), name="file-update"),
 ]
