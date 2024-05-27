90b2d62e92b9a1066bc00669fe59b8db19758bd3
Eklavaya <eklavaya2208@gmail.com>
2024-05-27 19:59:09 +0530

    updated billing party to foreign key in workorder

--- workorder/models.py
+++ workorder/models.py
@@ -3,13 +3,14 @@ from django.db import models
 from accounts.models import User
 from billing.models import Invoice
 from fleet.models import Machine
+from utils.models import Customer
 from fleet_commerce.mixin import AuthorTimeStampedModel
 
 
 class WorkOrder(AuthorTimeStampedModel):
     work_order_number = models.CharField(max_length=100)
     agreement_date = models.DateField()
-    billing_party = models.CharField(max_length=100)
+    billing_party = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="billing_party")
     contract_start_date = models.DateField()
     contract_end_date = models.DateField()
     machine = models.ManyToManyField(Machine, on_delete=models.CASCADE, related_name="machines")
--- workorder/serializers.py
+++ workorder/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from billing.serializers import InvoiceSerializer
 from fleet.serializers import MachineSerializer
 from accounts.serializers import UserSerializer
+from utils.serializers import CustomerSerializer
 
 from .models import WorkOrder
 
@@ -11,6 +12,7 @@ class WorkOrderSerializer(serializers.ModelSerializer):
     machines = serializers.SerializerMethodField()
     invoices = serializers.SerializerMethodField()
     resource_details = serializers.SerializerMethodField()
+    billing_party = serializers.SerializerMethodField()
 
     def get_machines(self, obj):
         machines = obj.machines.filter(is_deleted=False).all()
@@ -24,6 +26,10 @@ class WorkOrderSerializer(serializers.ModelSerializer):
         resources = obj.resource_details.filter(is_deleted=False).all()
         return UserSerializer(resources, many=True).data
 
+    def get_billing_party(self, obj):
+        billing_party = obj.billing_party.filter(is_deleted=False).all()
+        return CustomerSerializer(billing_party, many=True).data
+        
     class Meta:
         model = WorkOrder
         fields = "__all__"  # Adjust fields as necessary
