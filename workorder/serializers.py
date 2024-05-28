07ce33530e1e7d15d1ed96c8a04550a4a0158fa7
Eklavaya <eklavaya2208@gmail.com>
2024-05-28 16:30:11 +0530

    minor bug fixes

--- workorder/serializers.py
+++ workorder/serializers.py
@@ -36,15 +36,15 @@ class WorkOrderSerializer(serializers.ModelSerializer):
     )
 
     def get_machine(self, obj):
-        machines = obj.machine_set.filter(is_deleted=False).all()
-        return MachineSerializer(machines, many=True).data
+        machine = obj.machine.filter(is_deleted=False).all()
+        return MachineSerializer(machine, many=True).data
 
     def get_invoices(self, obj):
         invoices = obj.invoices_set.filter(is_deleted=False).all()
         return InvoiceSerializer(invoices, many=True).data
 
     def get_resource_details(self, obj):
-        resources = obj.resource_details_set.filter(is_deleted=False).all()
+        resources = obj.resource_details.filter(is_deleted=False).all()
         return UserSerializer(resources, many=True).data
 
     def get_customer(self, obj):
