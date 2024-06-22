2c394668617b02db5cff1a2d651ebf063253bc50
te-dious <tanmay@qoala.id>
2024-06-22 15:06:21 +0530

    fixes

--- stock/serializers.py
+++ stock/serializers.py
@@ -10,24 +10,16 @@ from .models import SparePart
 
 
 class SparePartSerializer(DynamicFieldSerializerMixin, serializers.ModelSerializer):
-    work_order = serializers.SerializerMethodField()
-    machine = serializers.SerializerMethodField()
+    work_order_obj = serializers.SerializerMethodField()
+    machine_obj = serializers.SerializerMethodField()
 
-    work_order_id = serializers.PrimaryKeyRelatedField(
-        queryset=WorkOrder.objects.all(), source="work_order", write_only=True
-    )
-
-    machine_id = serializers.PrimaryKeyRelatedField(
-        queryset=Machine.objects.all(), source="machine", write_only=True
-    )
-
-    def get_work_order(self, obj):
+    def get_work_order_obj(self, obj):
         work_order = obj.work_order
         if not work_order or work_order.is_deleted:
             return None
         return WorkOrderSerializer(work_order).data
 
-    def get_machine(self, obj):
+    def get_machine_obj(self, obj):
         machine = obj.machine
         if not machine or machine.is_deleted:
             return None
